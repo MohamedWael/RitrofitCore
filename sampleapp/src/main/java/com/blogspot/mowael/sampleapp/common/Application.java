9e6631f89b4e05caeff90fa847d1371dc7da3132
Mohamed Wael <mohamed.wael@v-apps.co>
2018-09-29 17:09:10 +0200

    adding debugger boolean

--- sampleapp/src/main/java/com/blogspot/mowael/sampleapp/common/Application.java
+++ sampleapp/src/main/java/com/blogspot/mowael/sampleapp/common/Application.java
@@ -1,12 +1,13 @@
 package com.blogspot.mowael.sampleapp.common;
 
 import com.blogspot.mowael.retrofitcore.RetrofitBase;
+import com.blogspot.mowael.sampleapp.BuildConfig;
 
 public class Application extends android.app.Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        RetrofitBase.initialize(Constants.URL);
+        RetrofitBase.initialize(Constants.URL, BuildConfig.DEBUG);
     }
 }
